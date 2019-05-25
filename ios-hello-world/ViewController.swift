54e12c568d8570686334ef5571cc1c07f270d048
Yingying Xia <yxia@Yingyings-MacBook-Pro.local>
2019-05-24 22:01:30 -0700

    Initial Commit

---
+++ ios-hello-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-hello-world
+//
+//  Created by Yingying Xia on 5/24/19.
+//  Copyright © 2019 yyxia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
